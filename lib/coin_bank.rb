ad7166063c224c0d4de93615aaa81c309ddf4f46
Artyom <artofyear@gmail.com>
2020-07-12 17:21:18 +0300

    reorganize code allow adding coin and item separately

---
+++ lib/coin_bank.rb
@@ -0,0 +1,54 @@
+module CoinBank
+
+  # def initialize()  
+  # end
+  private
+	def save_temp_coins_to_bank
+		@bank_coins.merge!(@operative_coins) { |k, o, n| o + n }
+	end
+
+	def return_coins_from_bank( coins )
+		@bank_coins.merge!(coins) { |k, o, n| o - n }
+	end
+
+
+  	def return_coins_with_sound coins
+		puts "** Sound of falling coins:"
+		coins.each do |k, v|
+			puts "** #{ v } coin#{'s' if v > 1 } of #{ get_coin(k) }"
+		end
+	end
+
+	def get_coin type
+		"#{type}#{type.to_f < 1 ? 'C' : '$'}"
+	end
+
+	def reset_temporary_bank
+		@operative_coins.clear
+		@operative_sum = 0
+		@item = nil
+	end
+
+	def get_change
+		remaining_change = balance
+		coins_to_return = {}
+		# move coins from temporary storage to the main coin bank
+		# covers bug if by user temporary coins can save vendor purchase if not enough coins in the main bank
+		save_temp_coins_to_bank
+
+		# check bank from the biggest money type to lowest 
+		@bank_coins.reverse_each do |type, num|
+			if num != 0
+				coin_value = type.to_f
+				coins_count = (balance / coin_value).floor
+				next if coins_count == 0
+				# if coins left set count result else set all the coins
+				coins_to_return[type] = coins_count < num ? coins_count : num
+				remaining_change -= coin_value * coins_count
+				break if remaining_change < 0.25 # lower than the lowest coin
+			end
+		end
+		return coins_to_return, remaining_change
+	end
+ 
+end
--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,113 +1,121 @@
+require_relative './coin_bank'
+
 class VendingMachine
-	def initialize() 
+	include CoinBank
+	attr_accessor  :items, :item, :balance
+	def initialize()
 	  #@items = items
 		@items = { 
 			'001' => { name: 'Cocoa Cola', price: 5.99, units: 3 },
-			'001' => { name: 'Banana Energy Bar', price: 19.99, units: 3 },
-			'001' => { name: 'Office mouse', price: 9.99, units: 3 },
-			'001' => { name: 'AA Batteries pack', price: 9.99, units: 3 }
+			'002' => { name: 'Banana Energy Bar', price: 19.99, units: 3 },
+			'003' => { name: 'Office mouse', price: 9.99, units: 3 },
+			'004' => { name: 'AA Batteries pack', price: 9.99, units: 3 }
 		}
+		# @item = nil
 		# store all the vending machine money with all available types by increased order
 		@bank_coins = {"0.25" => 5, "0.5" => 5, "1" => 5, "2" => 5, "5" => 5}
 		@operative_sum = 0 # store transaction sum
 		@operative_coins = Hash.new{0} # store transactions coins
 	end
-	
-	# pay 
-	def vend(code, paid)
-		item = items[code] # [code.to_sym]
 
+	def select_item( code = nil )
+		@item = items[code.to_s.rjust(3, '0')] || item # [code.to_sym]
 		if item
-			@operative_coins[paid.to_s] = paid
-			return if out_of_stock? item
-			return if illegal_sum? paid
-			@operative_sum += paid
-			price = item[:price].to_f
-			balance = ( @operative_sum - price ).round(2)
-			if @operative_sum >= price
-				remaining_change = balance
-				coins_to_return = {}
-				# check bank from the biggest money type to lowest 
-				@bank_coins.reverse_each do |type, num|
-					if num != 0
-						coin_value = type.to_f
-						coins_count = (balance / coin_value).floor
-						next if coins_count == 0
-						# if coins left set count result else set all the coins
-						coins_to_return[type] = coins_count < num ? coins_count : num
-						remaining_change -= coin_value * coins_count
-						break if remaining_change < 0.25 # lower than the lowest coin
-					end
-				end
-				if remaining_change > 0.25 # not enough chabge to return
-					return_coins_with_sound @operative_coins
-					reset_temporary_bank
-					"Our apologies, the machine is out of charge"
-				else # give the item
-					# get real change # clear not supported coins
-					final_change = balance - remaining_change
-
-					# remove coins from bank for change
-					@bank_coins.merge!(coins_to_return) { |k, o, n| o - n }
-					# move coins from temporary storage to the main coin bank
-					@bank_coins.merge!(@operative_coins) { |k, o, n| o + n }
-					
-					reset_temporary_bank
-					item[:units] -= 1
-					puts "1 item purchased"
+			if in_stock? item
+				if @operative_sum > 0
+					release_item if correct_sum_paid?
+				else # only code added
 					puts item[:name]
-					if final_change != 0 # return change if exist
-						puts "Your change is #{final_change}"
-						return_coins_with_sound coins_to_return
-					end
-					"Thank you for your purchase!"
+					"Price: #{item[:price]}"
 				end
-			else
-				puts "Product: #{item[:name]}"
-				puts "Price: #{price}"
-				puts "Inserted: #{get_coin(@operative_sum)}"
-				"Outstanding balance: #{ balance }"
 			end
 		else
 			'Please add a valid item code'
 		end
- 	end
- 
+	end
+
+	def insert_coin( paid )
+		coin = paid.to_f
+		if legal_coin? coin
+			@operative_coins[paid.to_s] += 1
+			@operative_sum += coin
+			if item
+				select_item
+			else
+				"Inserted: #{get_coin(@operative_sum)}"
+			end
+		end
+	end
+
+
 	private
- 
- 		attr_reader :items, :bank
 
- 		def return_coins_with_sound coins
- 			puts "Sound of falling coins:"
- 			coins.each do |k, v|
-				puts "#{ v } coins of #{ k }#{ get_coin(k) }"
+		def release_item
+			coins_to_return, remaining_change = get_change
+			if enough_change? remaining_change # give the item
+				# get real change # clear not supported coins
+				final_change = balance - remaining_change
+				# remove coins from bank for change
+				return_coins_from_bank coins_to_return
+
+				reset_temporary_bank
+				item[:units] -= 1
+				puts "1 item purchased"
+				puts item[:name]
+				if final_change != 0 # return change if exist
+					puts "Your change is #{final_change}"
+					return_coins_with_sound coins_to_return
+				end
+				"Thank you for your purchase!"
 			end
- 		end
+		end
 
- 		def get_coin type
- 			"#{type}#{type.to_f < 1 ? 'C' : '$'}"
- 		end
 
- 		def reset_temporary_bank
- 			@operative_coins.clear
- 			@operative_sum = 0
- 		end
+		def legal_coin? coin
+			if [0.25, 0.5, 1.0, 2.0, 5.0].include?(coin) # 25c 50c 1$ 2$ 5$
+				true
+			else
+				return_coins_with_sound({coin => 1})
+				puts 'The coin not recognized, machine accepts only (25c 50c 1$ 2$ 5$) coins'
+				false
+			end
+		end
 
- 		def illegal_sum? paid
-			unless %w{0.25 0.5 1 2 5}.include?(paid) # 25c 50c 1$ 2$ 5$ 
+		def in_stock? item
+			if item[:units] > 0
+				true
+			else
 				return_coins_with_sound @operative_coins
 				reset_temporary_bank
-				return 'The coin not recognized, machine accepts only (25c 50c 1$ 2$ 5$) coins'
+				puts "#{item[:name]} is out of stock"
+				puts "Would you like to buy another item?"
+				false
 			end
- 		end
+		end
 
- 		def out_of_stock? item
- 			if item.units == 0
+		def correct_sum_paid?
+			price = item[:price].to_f
+			@balance = ( @operative_sum - price ).round(2)
+			if @operative_sum >= price
+				true
+			else
+				puts "Product: #{item[:name]}"
+				puts "Price: #{price}"
+				puts "Inserted: #{get_coin(@operative_sum)}"
+				puts "Outstanding balance: #{ balance }"
+				false
+			end
+		end
+
+		def enough_change? 
+			if remaining_change > 0.25 # not enough change to return
 				return_coins_with_sound @operative_coins
+				return_coins_from_bank @operative_coins
 				reset_temporary_bank
- 				puts "#{item.[:name]} is out of stock"
- 				return "Would you like to buy another item?"
- 			end
- 		end
-
+				puts "Our apologies, the machine is out of charge"
+				false
+			else
+				true
+			end
+		end
 end
